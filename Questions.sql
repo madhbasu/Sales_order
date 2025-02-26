5193cd03358a14272ab9fa7b6da5968b850dd796
Madhushree Basu <madhushreebasu23@gmail.com>
2025-02-26 16:12:20 +0530

    Create Questions.sql

---
+++ Questions.sql
@@ -0,0 +1,52 @@
+select * from customers
+select * from products
+select * from sales_order
+
+1) Find the average sales order price based on deal size
+
+2) Find total no of orders per each day. Sort data based on highest orders.
+
+3) Display the total sales figure for each quarter. 
+Represent each quarter with their respective period
+
+4) Identify how many cars, Motorcycles, trains and ships are available in the inventory.
+Treat all type of cars as just "Cars".
+
+5) Identify the vehicles in the inventory which are short in number.
+Shortage of vehicle is considered when there are less than 10 vehicles.
+
+6) Find the countries which have purchased more than 500 motorcycles.
+
+7) Find the orders where the sales amount is incorrect.
+
+8) Fetch the total sales done for each day.
+
+9) Fetch the top 3 months which have been doing the lowest sales.
+
+
+10) Find total no of orders per each day of the week (monday to sunday). 
+Sort data based on highest orders.
+
+11) Find out the vehicles which was sold the most and which was sold the least. 
+Output should be a single record with 2 columns. 
+One column for most sold vehicle and other for least sold vehicle.
+
+12) Find all the orders which do not belong to customers from USA and are still in process.
+
+13) Find all orders for Planes, Ships and Trains which are neither Shipped nor In Process nor Resolved.
+
+14) Find customers whose phone number has either parenthesis "()" or a plus sign "+".
+
+15) Find customers whose phone number does not have any space.
+
+16) Fetch all the orders between Feb 2003 and May 2003 where the quantity ordered was an even number.
+
+17) Find orders which sold the product for price higher than its original price.
+
+18) Find the average sales order price
+
+19) Count total no of orders.
+
+20) Find the total quantity sold.
+
+21) Fetch the first order date and the last order date.
